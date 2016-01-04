10e54b6912599576d9264cf0cd27227809542e87
jmattefs <josephmattefs@gmail.com>
2016-01-04 14:38:14 -0600

    ot earnings

---
+++ scripts/new.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var wageA;
+var wageB;
+var hoursA;
+var hoursB;
+var OTwageA;
+var OTwageB;
+var earningsA;
+var earningsB;
+var OTearningsA;
+var OTearningsB;
+var totalearningsA;
+var totalearningsB;
+var earnings40A;
+var earnings40B;
+
+wageA = 17.24;
+wageB = 23.43;
+hoursA = 34;
+hoursB = 47;
+OTwageA = 25.86;
+OTwageB = 35.15;
+earningsA = wageA * hoursA;
+earningsB = wageB * hoursB;
+OTearningsA = OTwageA * (hoursA - 40);
+OTearningsB = OTwageB * (hoursB - 40);
+totalearningsA = earnings40A + OTearningsA;
+totalearningsB = earnings40B + OTearningsB;
+earnings40A = wageA * 40;
+earnings40B = wageB * 40;
+
+if (hoursA > 40){
+    console.log("totalearningsA=", totalearningsA);
+}else {
+	console.log("earningsA=", earningsA);
+}
+if (hoursB > 40){
+    console.log("totalearningsB=", totalearningsB);	
+}else{
+    console.log("earningsB=", earningsB);	
+}
--- scripts/one.js
+++ scripts/one.js
@@ -1,12 +1,56 @@
 "use strict";
 
-var wage;
-var hours;
-var earnings;
-wage = 22.5;
-hours = 37;
-earnings = wage * hours;
+var wageA;
+var hoursA;
+var earningsA;
+wageA = 17.24;
+hoursA = 34;
+earningsA = wageA * hoursA;
 
-console.log("wage= ", wage);
-console.log("hours= ", hours);
-console.log("wage*hours= ", earnings);
+console.log("wageA= ", wageA);
+console.log("hoursA= ", hoursA);
+console.log("earningsA= ", earningsA);
+console.log("totalearningsA=", totalearningsA);
+
+var overtimeA;
+overtimeA = ((wageA * 1.5) * (hoursA - 40));
+var totalearningsA;
+totalearningsA = earningsA + overtimeA;
+
+var wageB;
+var hoursB;
+var earningsB;
+wageB = 23.43;
+hoursB = 47;
+earningsB = wageB * hoursB;
+
+console.log("wageB= ", wageB);
+console.log("hoursB= ", hoursB);
+console.log("earningsB= ", earningsB);
+console.log("totalearningsB=", totalearningsB);
+
+var overtimeB;
+overtimeB = (wageB * 1.5) * (hoursB - 40);
+var totalearningsB;
+totalearningsB = earningsB + overtimeB;
+
+if (hoursA > 40) {
+    earningsA = (wageA * 40);
+    overtimeA = ((wageA * 1.5) * (hoursA - 40));
+    totalearningsA = (earningsA + overtimeA);
+}
+ else{
+    earningsA = wageA * hoursA;
+    overtimeA = 0;
+    totalearningsA = earningsA;
+	}
+
+if (hoursB > 40){
+    earningsB = (wageB * 40);
+    overtimeB = ((wageB * 1.5) * (hoursB - 40));
+    totalearningsB = (earningsB + overtimeB);
+}
+ else{
+    earningsB = wageB * hoursB;
+    overtimeB = 0;
+    totalearningsB = earningsB};
