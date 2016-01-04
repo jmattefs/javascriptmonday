2bbfd46919561c37afe95db533ac616f53f274e7
jmattefs <josephmattefs@gmail.com>
2016-01-04 11:06:39 -0600

    finished 2nd task

---
+++ scripts/one.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var wage;
+var hours;
+var earnings;
+wage = 22.5;
+hours = 37;
+earnings = wage * hours;
+
+console.log("wage= ", wage);
+console.log("hours= ", hours);
+console.log("wage*hours= ", earnings);
