f4c4ab47890ee233710fd653a12b2e84f437102a
jmattefs <josephmattefs@gmail.com>
2016-01-07 08:45:52 -0600

    document prints message displaying earnings

--- scripts/newfunction.js
+++ scripts/newfunction.js
@@ -1,6 +1,11 @@
 "use strict";
+function acquireData(message) {
+	var acquire;
+	acquire = prompt(message);
+	return acquire;
+}
 
-function reg(wage, hours) {
+function regCalc(wage, hours) {
 	var regResult
 	regResult = wage * hours;
 	var maxRegHours
@@ -13,10 +18,8 @@ function reg(wage, hours) {
 		return fortyResult;
 		}
 }
-console.log(reg(17.24,34));
-console.log(reg(23.43,47));
 
-function ot(wage, hours) {
+function otCalc(wage, hours) {
 	var hoursBeforeOT
 	hoursBeforeOT = 40
 	var otMultiplier;
@@ -34,9 +37,46 @@ function ot(wage, hours) {
 		return otResult;
 	}
 }
-console.log(ot(17.24,34));
-console.log(ot(23.43,47));
 
-console.log(reg(17.24,34) + ot(17.24,34));
-console.log(reg(23.43,47) + ot(23.43,47));
+
+function addRegOT(reg, ot) {
+	var result;
+	result = reg + ot;
+	return result;
+}
+
+function calcMessage(userName, userHours, userTotal) {
+	var finalMessage;
+	finalMessage = userName + ", you worked " + userHours + " hours, and earned $" + userTotal + " this week. ";
+	return finalMessage;
+}
+
+function main () {
+	var nameMessage;
+	var hoursMessage;
+	var wageMessage;
+	var name;
+	var hours;
+	var wage;
+	var total;
+	var reg;
+	var ot;
+	var message;
+
+	
+	nameMessage = "What is your name?";
+	hoursMessage = "How many hours did you work this week?";
+	wageMessage = "What is your hourly wage?";
+	name = acquireData(nameMessage);
+	hours = acquireData(hoursMessage);
+	wage = acquireData(wageMessage);
+	reg = regCalc(wage, hours);
+	ot = otCalc(hours, wage);
+	total = addRegOT(reg, ot);
+	message = calcMessage(name, hours, total);
+	document.write(message);
+	
+	
+}
+main()
 
